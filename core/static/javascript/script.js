aaf85df6f80601710851434dbd4b181ecb7221ca
michellssgui <michellss.gui@gmail.com>
2023-02-26 11:45:49 -0300

    michelinux - layout finalizado

---
+++ core/static/javascript/script.js
@@ -0,0 +1,9 @@
+$('.nav-suave a[href^="#"]').on('click', function(e) {
+	e.preventDefault();
+	var id = $(this).attr('href'),
+			targetOffset = $(id).offset().top;
+			
+	$('html, body').animate({ 
+		scrollTop: targetOffset - 100
+	}, 500);
+});
